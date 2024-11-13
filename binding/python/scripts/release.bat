4d435ba7dac3ffeee8d9fe5f53f95969eb9fe16b
timerke <timerke@mail.ru>
2024-11-13 15:56:40 +0300

    Added scripts to release

---
+++ binding/python/scripts/release.bat
@@ -0,0 +1,22 @@
+cd ..
+setlocal enableextensions
+
+if defined TWINE_USERNAME (echo found username) else (exit)
+if defined TWINE_PASSWORD (echo found password) else (exit)
+
+if exist build-venv rd /s/q build-venv
+if exist dist rd /s/q dist
+if exist src rd /s/q src
+if exist tests rd /s/q tests
+
+md src
+xcopy /i /s /e /h /y .\bindy .\src\bindy
+md tests
+
+python -m venv build-venv
+build-venv\Scripts\python -m pip install --upgrade pip
+build-venv\Scripts\python -m pip install build
+build-venv\Scripts\python -m build
+
+build-venv\Scripts\python -m pip install twine
+build-venv\Scripts\python -m twine upload dist\* 
---
+++ binding/python/scripts/release.sh
@@ -0,0 +1,22 @@
+: "${TWINE_USERNAME:?TWINE_USERNAME empty, exiting}"
+: "${TWINE_PASSWORD:?TWINE_PASSWORD empty, exiting}"
+cd ..
+
+[ -d "./dist" ] && rm -rf ./dist
+[ -d "./src" ] && rm -rf ./src
+[ -d "./tests" ] && rm -rf ./tests
+[ -d "./build-venv" ] && rm -rf ./build-venv
+
+mkdir src
+cp -R ./bindy ./src/bindy
+mkdir tests
+
+python3 -m venv build-venv
+source ./build-venv/bin/activate
+
+python -m pip install --upgrade pip
+python -m pip install build
+python -m build
+
+python -m pip install twine
+python -m twine upload dist/* 

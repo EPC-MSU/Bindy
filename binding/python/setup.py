fd9ede20b53045ab04f98d713123fdcafcdf498d
timerke <timerke@mail.ru>
2024-11-13 16:38:58 +0300

    Fixed version

--- binding/python/setup.py
+++ binding/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(name="bindy",
-      version="1.0.0-3",
+      version="1.0.0",
       description="A lightweight cross-platform framework for cloud service",
       url="https://github.com/EPC-MSU/Bindy",
       author="EPC MSU",

0b3db0b6c68bb8630274acc664a33a48b648e455
timerke <timerke@mail.ru>
2024-11-13 16:36:04 +0300

    Fixed release

--- binding/python/scripts/release.bat
+++ binding/python/scripts/release.bat
@@ -8,10 +8,12 @@ if exist build-venv rd /s/q build-venv
 if exist dist rd /s/q dist
 if exist src rd /s/q src
 if exist tests rd /s/q tests
+if exist LICENSE del LICENSE
 
 md src
 xcopy /i /s /e /h /y .\bindy .\src\bindy
 md tests
+copy ..\..\LICENSE LICENSE
 
 python -m venv build-venv
 build-venv\Scripts\python -m pip install --upgrade pip
